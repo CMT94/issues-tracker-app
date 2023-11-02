70913285365a3c58fc1a5dd071151a486bdec773
Clément M <cmt94.dev@gmail.com>
2023-11-02 11:04:38 +0100

    install and set up radix ui theme

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { Button } from "@radix-ui/themes";
+
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button color="cyan" variant="soft">
+        New Issue
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
